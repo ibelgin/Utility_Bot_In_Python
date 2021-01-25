a744532ebd0e88e35f45f7f3d006df60cfd104da
karthikeyan-17 <73576856+karthikeyan-17@users.noreply.github.com>
2021-01-25 20:42:38 +0530

    Add files via upload

---
+++ Front end.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root = Tk()
+
+def send():
+
+    send="You :" +e.get()
+    txt.insert(END,"\n"+send)
+    if(e.get()=="hi"):
+        txt.insert(END,"\n"+"Bot : hello")
+
+
+txt =Text(root,bg="light green",width=50)
+txt.grid(row=0,column=0,columnspan=2)
+e=Entry(root,width=50)
+send=Button(root,text="Send",bg="brown",width=15,command=send).grid(row=1,column=1)
+e.grid(row=1,column=0)
+root.title("Chartbot")
+root.mainloop()
